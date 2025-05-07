e33a6d7ee72ab8de42029da2bb2fd797cc43046e
Luciano Portales <lportales85@gmail.com>
2025-05-07 01:09:50 -0400

    actualizacion

--- file/love.js
+++ file/love.js
@@ -168,8 +168,8 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText("Haz clic, por fi ♥", 30, -5);
+            ctx.fillText("¡Mi princesa de cumpleaños!", 28, 10);
             ctx.restore();
         },
         clear: function() {
